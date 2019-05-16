f5c0e0373270bdfffcfc00cdf56e2a21e5bd03ea
Alexander Granin <graninas@gmail.com>
2019-05-16 13:26:48 +0700

    Meteor counter app fixed.

--- app/MeteorCounter/Main.hs
+++ app/MeteorCounter/Main.hs
@@ -8,10 +8,11 @@ import qualified Hydra.Language as L
 import           Hydra.Prelude
 import qualified Hydra.Runtime  as R
 
-type MTime = Int
+type Time = Int
 
 data Meteor = Meteor
-  { size :: Int
+  { _size :: Int
+  , _mass :: Int
   }
   deriving (Show, Eq, Ord)
 
@@ -22,55 +23,82 @@ data Region
   | SouthWest
   deriving (Show, Eq, Ord)
 
-type Meteors = D.StateVar (Map.Map MTime Meteor)
+type Meteors = D.StateVar (Set.Set Meteor)
 
 type Catalogue = Map.Map Region Meteors
 
 data AppState = AppState
-  { catalogue :: D.StateVar Catalogue
+  { _catalogue :: Catalogue
+  , _published :: D.StateVar (Set.Set (Region, Meteor))
   }
 
 
 initState :: L.AppL AppState
 initState = do
-  ne <- L.newVarIO Map.empty
-  nw <- L.newVarIO Map.empty
-  se <- L.newVarIO Map.empty
-  sw <- L.newVarIO Map.empty
-  let catalogueMap = Map.fromList
+  ne <- L.newVarIO Set.empty
+  nw <- L.newVarIO Set.empty
+  se <- L.newVarIO Set.empty
+  sw <- L.newVarIO Set.empty
+
+  let catalogue = Map.fromList
         [ (NorthEast, ne)
         , (NorthWest, nw)
         , (SouthEast, se)
         , (SouthWest, sw)
         ]
-  catalogue <- L.newVarIO catalogueMap
-  pure $ AppState catalogue
 
-meteorCounter :: AppState -> L.LangL ()
-meteorCounter st = pure ()
+  publised <- L.newVarIO Set.empty
+  pure $ AppState catalogue publised
 
 getRandomMeteor :: L.LangL Meteor
-getRandomMeteor = Meteor <$> L.getRandomInt (1, 100)
+getRandomMeteor = do
+  size <- L.getRandomInt (1, 100)
+  mass <- L.getRandomInt (size * 1000, size * 10000)
+  pure $ Meteor size mass
 
-getRandomMilliseconds :: L.LangL MTime
+getRandomMilliseconds :: L.LangL Time
 getRandomMilliseconds = (* 1000) <$> L.getRandomInt (0, 3000)
+
+withRandomDelay :: L.LangL () -> L.LangL ()
+withRandomDelay action = do
+  getRandomMilliseconds >>= L.delay
+  action
+
+publishMeteor :: AppState -> (Region, Meteor) -> L.LangL ()
+publishMeteor st meteor =
+  L.atomically $ L.modifyVar (_published st) $ Set.insert meteor
 
 meteorShower :: AppState -> Region -> L.LangL ()
 meteorShower st region = do
-  getRandomMilliseconds >>= L.delay
   meteor <- getRandomMeteor
-  L.logInfo $ "[MS] " <> " a new meteor appeared at " <> show region <> ": " <> show meteor
-  meteorShower st region
+  L.logInfo $ "A new meteor appeared at " <> show region <> ": " <> show meteor
+  publishMeteor st (region, meteor)
+
+countMeteor :: AppState -> (Region, Meteor) -> L.LangL ()
+countMeteor st (region, meteor) =
+  case Map.lookup region (_catalogue st) of
+    Nothing -> L.logError $ "Region not found: " <> show region
+    Just r  -> do
+      L.atomically $ L.modifyVar r $ Set.insert meteor
+      L.logInfo $ "A new meteor tracked at " <> show region <> ": " <> show meteor
+
+meteorCounter :: AppState -> L.LangL ()
+meteorCounter st = do
+  untracked <- L.atomically $ do
+    ps <- L.readVar (_published st)
+    L.writeVar (_published st) Set.empty
+    pure $ Set.toList ps
+  mapM_ (countMeteor st) untracked
 
 meteorsMonitoring :: L.AppL ()
 meteorsMonitoring = do
   L.logInfo "Starting app..."
   st <- initState
-  L.process $ meteorCounter st
-  L.process $ meteorShower st NorthEast
-  L.process $ meteorShower st NorthWest
-  L.process $ meteorShower st SouthEast
-  L.process $ meteorShower st SouthWest
+  L.process $ forever $ meteorCounter st
+  L.process $ forever $ withRandomDelay $ meteorShower st NorthEast
+  L.process $ forever $ withRandomDelay $ meteorShower st NorthWest
+  L.process $ forever $ withRandomDelay $ meteorShower st SouthEast
+  L.process $ forever $ withRandomDelay $ meteorShower st SouthWest
 
 loggerCfg :: D.LoggerConfig
 loggerCfg = D.LoggerConfig
