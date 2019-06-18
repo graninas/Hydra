3f4e0edbcab0ec7f89c216f4e4a66a64771a86e7
Alexander Vershilov <alexander.vershilov@gmail.com>
2019-06-19 00:50:58 +0300

    Generalize FT example to keep it unbounded from IO and STM.
    It's possible to introduce classes similar to the ones used
    in free language and have generalized implemntation in the
    FT case.
    The core idea is to introduce a simple class type and then
    you can introduce instance for AppM. Then you can control
    the implementation by passing differrent wrappers instead
    of AppM. It's possible to generalize even more, but in general
    it's coutner-productive.

--- src/Hydra/Core/ControlFlow/FTL.hs
+++ src/Hydra/Core/ControlFlow/FTL.hs
@@ -4,3 +4,11 @@ import           Hydra.Prelude
 
 class Monad m => ControlFlowL m where
   delay :: Int -> m ()
+
+instance ControlFlowL IO where
+  delay = threadDelay
+  {-# INLINE delay #-}
+
+instance ControlFlowL (ReaderT e IO) where
+  delay = lift . threadDelay
+  {-# INLINE delay #-}
