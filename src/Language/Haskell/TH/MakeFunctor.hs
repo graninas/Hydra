73c7ef107ff2cbb80594e9647d5e3a2d7920f059
Alexander Granin <graninas@gmail.com>
2019-07-12 15:52:05 +0700

    MakeFunctor reworked. Enq license deleted.

--- src/Language/Haskell/TH/MakeFunctor.hs
+++ src/Language/Haskell/TH/MakeFunctor.hs
@@ -1,35 +1,32 @@
 module Language.Haskell.TH.MakeFunctor where
 
+import qualified Data.List                    as L
 import           Hydra.Prelude
-import qualified Data.List as L
 import           Language.Haskell.TH
 import           Language.Haskell.TH.Datatype
 
 makeFunctorInstance :: Name -> Q [Dec]
 makeFunctorInstance name =
-    forM [1 :: Int] $ \_ -> instanceD (cxt []) (appT (conT $ mkName "Functor") (conT name)) [makeFmap name]
+  (: []) <$> instanceD (cxt []) (appT (conT $ mkName "Functor") (conT name)) [makeFmap name]
 
 makeFmap :: Name -> Q Dec
 makeFmap name = do
-    constructors <- datatypeCons <$> reifyDatatype name
-    funD (mkName "fmap") (makeFmapBody <$> constructors)
+  constructors <- datatypeCons <$> reifyDatatype name
+  funD (mkName "fmap") (map makeFmapBody constructors)
 
 makeFmapBody :: ConstructorInfo -> Q Clause
-makeFmapBody info = clause
-    [varP $ mkName "g", conP consName (varP <$> varNames)]
-    (normalB
-        (  foldApp
-        $  ConE consName
-        :  (VarE <$> L.init varNames)
-        ++ [UInfixE (VarE $ mkName "g") (VarE $ mkName ".") (VarE lastArg)]
-        )
-    )
+makeFmapBody info = do
+  let argsCount = length $ constructorFields info
+  let varNames  = [ mkName $ "a" <> show a | a <- [1 .. argsCount] ]
+  let consName  = constructorName info
+  let vars      = map VarE $ L.init varNames
+  let expr      = UInfixE (VarE $ mkName "g") (VarE $ mkName ".") (VarE $ last varNames)
+  clause
+    [ varP $ mkName "g"
+    , conP consName (map varP varNames)
+    ]
+    (normalB $ foldApp $ ConE consName : vars ++ [expr])
     []
-  where
-    lastArg  = last varNames
-    varNames = (\a -> mkName $ "a" <> show a) <$> [1 .. argNum]
-    consName = constructorName info
-    argNum   = length $ constructorFields info
 
 foldApp :: [Exp] -> Q Exp
 foldApp = pure . foldl1 AppE
