3b78b881b13fabafb554dd05a7c3bb53927fa624
Alexander Granin <graninas@gmail.com>
2020-03-03 01:16:38 +0700

    More improvements in Final Tagless.

--- src/Hydra/Framework/Cmd/Language.hs
+++ src/Hydra/Framework/Cmd/Language.hs
@@ -18,6 +18,9 @@ import           Data.Char
 import           Data.List
 import           Data.Default
 
+import           GHC.Generics
+import qualified GHC.Generics               as GS
+
 import qualified Hydra.Core.Language as L
 
 
@@ -37,11 +40,48 @@ type CmdHandlerL a = Free CmdHandlerF a
 
 
 
-
-
-
-
-
+-- data FieldType
+--   = FStr
+--   | FInt
+--   deriving (Show)
+--
+-- data FieldDef = FieldDef String FieldType
+--   deriving (Show)
+--
+-- class ToFieldDef1 f where
+--   toFieldDef1 :: Proxy f -> [FieldDef]
+--
+-- instance (ToFieldDef1 f) => ToFieldDef1 (GS.M1 D t f) where
+--   toFieldDef1 _ = toFieldDef1 (Proxy :: Proxy f)
+--
+-- instance (ToFieldDef1 f) => ToFieldDef1 (GS.C1 c f) where
+--   toFieldDef1 _ = toFieldDef1 (Proxy :: Proxy f)
+--
+-- instance (ToFieldDef1 k1, Selector s) => ToFieldDef1 (GS.M1 GS.S s k1) where
+--   toFieldDef1 _ = fs'
+--     where
+--       FieldDef fn ft : fs = toFieldDef1 (Proxy :: Proxy k1)
+--       fn' = GS.selName (undefined :: M1 S s k1 k)
+--       fs' = FieldDef fn' ft : fs
+--
+-- instance ToFieldDef1 (GS.K1 GS.R String) where
+--   toFieldDef1 _ = [FieldDef "" FStr]
+--
+-- instance ToFieldDef1 (GS.K1 GS.R Int) where
+--   toFieldDef1 _ = [FieldDef "" FInt]
+--
+-- instance
+--   (ToFieldDef1 f, ToFieldDef1 g)
+--   => ToFieldDef1 ((:*:) f g) where
+--   toFieldDef1 _ = toFieldDef1 (Proxy :: Proxy f) ++ toFieldDef1 (Proxy :: Proxy g)
+--
+-- defaultToFieldDef
+--   :: forall a
+--   . (Generic a, ToFieldDef1 (Rep a))
+--   => Proxy a
+--   -> [FieldDef]
+-- defaultToFieldDef _ = toFieldDef1 (Proxy :: Proxy (Rep a))
+--
 
 
 
@@ -67,24 +107,6 @@ userCmd cmd handler = liftF $ UserCmd fParse handler id
       Just stripped -> case readMaybe $ concat [cName, " ", stripped] of
         Nothing -> Left $ "Args not parsed: " +| concat [cName, " ", stripped] |+ ""
         Just r  -> Right r
-
-
-
--- userCmd
---   :: forall a
---    . (Read a, Data a, Default a)
---   => Text
---   -> (a -> L.LangL ())
---   -> CmdHandlerL ()
--- userCmd cmd handler = liftF $ UserCmd fParse handler id
---   where
---     cName = show @Text $ toConstr $ def @a
---     fParse :: Text -> Either Text a
---     fParse line = case T.stripPrefix cmd $ T.stripStart line of
---       Nothing -> Left $ "Cmd not parsed: " +|| (cmd, cName, line) ||+ ""
---       Just stripped  -> case readMaybe $ toString $ T.concat [cName, " ", stripped] of
---         Nothing -> Left $ "Args not parsed: " +| T.concat [cName, " ", stripped] |+ ""
---         Just r  -> Right r
 
 
 stdHandler
