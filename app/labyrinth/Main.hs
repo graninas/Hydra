faebb0f37f3d87fad54e1a80d4be456193544834
Alexander Granin <graninas@gmail.com>
2020-04-26 18:25:23 +0700

    New way of pre-rendering the labyrinth

--- app/labyrinth/Main.hs
+++ app/labyrinth/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Hydra.Prelude
+import           Labyrinth.Prelude
 
 import           System.Environment         (getArgs)
 import qualified Data.Text                  as T
@@ -25,34 +25,120 @@ loggerCfg = D.LoggerConfig
   }
 
 -- l r u d
-testLabyrinth = Map.fromList
-  [ ((0, 0), (Cell Wall Wall Wall Wall, NoContent))
-  , ((1, 0), (Cell Wall Wall NoWall Wall, Treasure))
-  , ((2, 0), (Cell Wall Wall Wall Wall, NoContent))
+testLabyrinth2 = Map.fromList
+  [ ((0, 0), (Cell (Monolith False) Wall Wall Wall, NoContent))
+  , ((1, 0), (Cell (Monolith False) Wall NoWall Wall, Treasure))
+  , ((2, 0), (Cell (Monolith False) Wall Wall Wall, NoContent))
 
   , ((0, 1), (Cell NoWall Wall Wall Wall, NoContent))
   , ((1, 1), (Cell NoWall NoWall NoWall NoWall, NoContent))
   , ((2, 1), (Cell Wall NoWall Wall Wall, NoContent))
 
-  , ((0, 2), (Cell Wall Wall Wall Wall, NoContent))
-  , ((1, 2), (Cell Wall Wall Wall NoWall, NoContent))
-  , ((2, 2), (Cell Wall Wall Wall Wall, NoContent))
+  , ((0, 2), (Cell Wall (Monolith False) Wall Wall, NoContent))
+  , ((1, 2), (Cell Wall (Monolith False) Wall NoWall, NoContent))
+  , ((2, 2), (Cell Wall (Monolith False) Wall Wall, NoContent))
+  ]
+
+testLabyrinth1 :: Labyrinth
+testLabyrinth1 = Map.fromList
+  [ ((0, 0), (Cell (Monolith False) (Monolith False) (Monolith False) (Monolith True), Treasure))
   ]
 
 
-initGameState :: L.AppL GameState
+renderLabyrinthSkeleton :: Int -> Int -> LabRender
+renderLabyrinthSkeleton maxX maxY = skeleton
+  where
+    p x y = (x, y)
+    rendMaxX = maxX * 2
+    rendMaxY = maxY * 2
+
+    genLeftMonolithCross   = [(p 0 y,        "┠") | y <- [0, 2..rendMaxY ], y > 1, y < rendMaxY - 1 ]
+    genRightMonolithCross  = [(p rendMaxX y, "┨") | y <- [0, 2..rendMaxY ], y > 1, y < rendMaxY - 1 ]
+    genTopMonolithCross    = [(p x 0,        "┯") | x <- [0, 2..rendMaxX ], x > 1, x < rendMaxX - 1 ]
+    genBottomMonolithCross = [(p x rendMaxY, "┷") | x <- [0, 2..rendMaxX ], x > 1, x < rendMaxX - 1 ]
+    genInternalCross       = [(p x y, "┼") | x <- [0, 2..rendMaxX - 1]
+                                           , y <- [0, 2..rendMaxY - 1]
+                                           , y > 1, y < rendMaxY - 1
+                                           , x > 1, x < rendMaxX - 1
+                                           ]
+    fullTemplate           = [(p x y, " ") | x <- [0..rendMaxX], y <- [0..rendMaxY]]
+
+    intersections =
+      [ (p 0 0,               "┏")
+      , (p rendMaxX 0,        "┓")
+      , (p 0 rendMaxY,        "┗")
+      , (p rendMaxX rendMaxY, "┛")
+      ]
+      ++ genLeftMonolithCross
+      ++ genRightMonolithCross
+      ++ genTopMonolithCross
+      ++ genBottomMonolithCross
+      ++ genInternalCross
+
+    skeleton = Map.union (Map.fromList intersections) (Map.fromList fullTemplate)
+
+printSkeleton :: Int -> Int -> LabRender -> AppL ()
+printSkeleton mx my skeleton = do
+  scenario $ putStrLn $ show skeleton
+
+  let rendMaxX = mx * 2
+  let rendMaxY = my * 2
+
+  let printAndMergeCells y row x = case Map.lookup (x, y) skeleton of
+        Nothing -> row <> "!"
+        Just c  -> row <> c
+
+  let printAndMergeRows y rows =
+        let row = foldl' (printAndMergeCells y) "" [0..rendMaxX]
+        in row : rows
+
+  let printedRows = foldr printAndMergeRows [] [0..rendMaxY]
+
+  let outputRows row = putStrLn row
+  scenario $ mapM_ outputRows printedRows
+
+
+-- ┓ ┯ ┯ ┯ ┏
+--
+-- ┨ ┼ ┼ ┼ ┠
+--
+-- ┨ ┼ ┼ ┼ ┠
+--
+-- ┨ ┼ ┼ ┼ ┠
+--
+-- ┛ ┷ ┷ ┷ ┗
+
+
+
+initGameState :: AppL GameState
 initGameState = do
-  lab       <- L.newVarIO testLabyrinth
-  labSize   <- L.newVarIO (3, 3)
-  pos       <- L.newVarIO (0, 0)
-  inv       <- Inventory <$> L.newVarIO False
-  treasure  <- L.newVarIO Nothing
-  fiinished <- L.newVarIO False
-  pure $ GameState lab labSize Map.empty pos inv treasure fiinished
+  printSkeleton 1 1 $ renderLabyrinthSkeleton 1 1
+  printSkeleton 2 2 $ renderLabyrinthSkeleton 2 2
+  printSkeleton 3 3 $ renderLabyrinthSkeleton 3 3
+  printSkeleton 4 4 $ renderLabyrinthSkeleton 4 4
+
+  let maxX = 1
+  let maxY = 1
+
+  labRenderVar <- newVarIO $ Map.fromList $ do
+    x <- [0..maxX*2]
+    y <- [0..maxY*2]
+    pure ((x, y), T.pack "")
+
+  let wormholes = Map.empty
+
+  labVar           <- newVarIO testLabyrinth1
+  labSizeVar       <- newVarIO (maxX, maxY)
+  posVar           <- newVarIO (0, 0)
+  inv              <- Inventory <$> newVarIO False
+  aboutLeaving     <- newVarIO Nothing
+  finished         <- newVarIO False
+
+  pure $ GameState labVar labSizeVar labRenderVar wormholes posVar inv aboutLeaving finished
 
 
 
-startApp :: L.AppL ()
+startApp :: AppL ()
 startApp = initGameState >>= app
 
 main :: IO ()
