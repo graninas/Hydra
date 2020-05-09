982ec55917a17119d681ce93c3baff9af714b7b1
Daneel Yaitskov <dyaitskov@gmail.com>
2020-05-09 16:34:11 -0700

    warnings top signature

--- lib/Hydra/src/Hydra/Core/Logger/Impl/HsLoggerChurchI.hs
+++ lib/Hydra/src/Hydra/Core/Logger/Impl/HsLoggerChurchI.hs
@@ -2,7 +2,12 @@ module Hydra.Core.Logger.Impl.HsLoggerChurchI where
 
 import           Hydra.Prelude
 
+import           Hydra.Core.Logger.Impl.HsLogger (HsLoggerHandle)
+import           Hydra.Core.Logger.Language (LoggerF)
 import qualified Hydra.Core.Logger.Impl.HsLoggerInterpreter as I
 
+
+
+runLoggerL :: Maybe HsLoggerHandle -> F LoggerF () -> IO ()
 runLoggerL (Just h) l = foldF (I.interpretLoggerF h) l
 runLoggerL Nothing  _ = pure ()
